9200692e3fc2eac87486d934836de96f02858165
iscChris <chris.stewart@intersystems.com>
2017-04-20 15:46:42 +0100

    Updated to end of Part 4 of series
    This is the state of the application at the end of Part 4

--- web/widgetmaster.js
+++ web/widgetmaster.js
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-20 08:29:50">
+<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-20 15:45:46">
 <CSP name="widgetmaster.js" application="/widgetsdirect/"><![CDATA[
 angular
-  .module('WidgetsDirect', [])
+  .module('WidgetsDirect', ['ngMaterial'])
    .controller('PageController', ['$q', '$scope', '$timeout', '$http', '$log', function($q, $scope, $timeout, $http, $log) {
 	
 	$scope.message = ""
