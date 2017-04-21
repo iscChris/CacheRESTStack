2080cf299ba85b110aa749bb7252179f65813d12
iscChris <chris.stewart@intersystems.com>
2017-04-21 09:37:41 +0100

    Parts 5 and 6
    This checkin will give you the application state up to the end of Part6

--- web/widgetmaster.js
+++ web/widgetmaster.js
@@ -1,11 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-20 15:45:46">
+<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-21 09:35:29">
 <CSP name="widgetmaster.js" application="/widgetsdirect/"><![CDATA[
 angular
   .module('WidgetsDirect', ['ngMaterial'])
    .controller('PageController', ['$q', '$scope', '$timeout', '$http', '$log', function($q, $scope, $timeout, $http, $log) {
 	
-	$scope.message = ""
+	$scope.message = "";
+	$scope.sortAsc = true;
+	
+	
 	
 	$http.get('/widgetsdirect/rest/Chris').then(
 	  function(response) { //success
