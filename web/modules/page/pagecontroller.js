03d05a5c34f23f06e8c0a65d466647e841f71aad
iscChris <chris.stewart@intersystems.com>
2017-04-25 14:38:55 +0100

    Part 9
    This contains the new REST class for Part 9, the controller changes to
    lookup the new service, and implements fromJSON() on User.Widget

--- web/modules/page/pagecontroller.js
+++ web/modules/page/pagecontroller.js
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-24 08:53:49">
+<Export generator="Cache" version="25" zv="Cache for Windows (x86-64) 2016.2 (Build 736U)" ts="2017-04-25 14:37:47">
 <CSP name="modules/page/pageController.js" application="/widgetsdirect/"><![CDATA[
 angular
   .module('WidgetsDirect', ['ngMaterial'])
@@ -13,10 +13,20 @@ angular
 	$http.get('/widgetsdirect/rest/Chris').then(
 	  function(response) { //success
 	    $scope.message = response.data.Message;
-	    $scope.widgets = response.data.Widgets;
+	    
       }
      ,function(response) { //failure
 	   $scope.message = "Couldn't get data :(";
+       }
+      );
+      
+      $http.get('/widgetsdirect/rest/widget/').then(
+	  function(response) { //success
+	   
+	    $scope.widgets = response.data.Widgets;
+      }
+     ,function(response) { //failure
+	   $scope.widgets = [];
        }
       );
 	   
